ea88a65691554dff695106077933eb6f97d02e4b
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-11-08 15:13:46 +0300

    added welcome page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
+let options = {
+
+};
+router.use(express.static("landingpage", options));
+
 // Welcome Page
-router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+router.get('/', forwardAuthenticated, (req, res) => res.sendFile('index.html', {root: './views/landing_page/'}));
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
