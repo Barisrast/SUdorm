806c58ee05733ea3989d389d7e3bc90cd5250ae7
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-11-08 10:44:22 +0300

    initializing app

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://barisrastgeldi:barisrastgeldisifre@cluster0.8a3cb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
