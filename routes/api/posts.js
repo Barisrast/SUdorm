4d3bf8b02f00ce39d432342437c2fd41dd32c798
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-11-22 16:06:27 +0300

    A route to post, user posts has been added to api.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,10 +7,37 @@ const Post = require("../../models/Post");
 const User = require("../../models/User");
 const checkObjectId = require("../../middleware/checkObjectId");
 
-// @router  GET api/posts
-// @desc    test route
-// @access  public
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  "/",
+  auth,
+  check("text", "Post has to include text").notEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-router.get("/", (req, res) => res.send("posts route"));
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
