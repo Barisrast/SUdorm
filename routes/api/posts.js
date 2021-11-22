1794f8751e6fcfac5bef939f82d70824396ad1e4
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-11-22 10:44:30 +0300

    model for posts has been added. And posts route of api has been initialized.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,11 @@
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+
+const Post = require("../../models/Post");
+const User = require("../../models/User");
+const checkObjectId = require("../../middleware/checkObjectId");
 
 // @router  GET api/posts
 // @desc    test route
