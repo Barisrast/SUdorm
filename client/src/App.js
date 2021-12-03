9da63766bc2adffce77926fed143974b23acf013
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-12-03 16:57:19 +0300

    reducers for authentication has been added.

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,25 @@ import "./App.css";
 import Landing from "./components/landing";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
+//redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Routes>
-      <Fragment>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-      </Fragment>
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Fragment>
+          <section className="container">
+            <Alert />
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </section>
+        </Fragment>
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
