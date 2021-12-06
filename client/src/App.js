cc83f1677ad9e33e61d3bd565404c6f39e8ac521
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-12-06 15:44:09 +0300

    added a notfound warning page to render when users tries to access an invalid route.

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
 import Post from "./components/post/Post";
+import NotFound from "./components/layout/NotFound";
 
 //redux
 import { Provider } from "react-redux";
@@ -64,6 +65,7 @@ const App = () => {
               path="posts/:id"
               element={<PrivateRoute component={Post} />}
             />
+            <Route path="/*" element={<NotFound />} />
           </Fragment>
         </Routes>
       </Router>
