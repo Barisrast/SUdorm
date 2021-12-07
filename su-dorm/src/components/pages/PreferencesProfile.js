5293ee46c9a63c0701d71e16c0b973146437be81
= <=>
2021-12-07 15:49:55 +0300

    some changes

---
+++ su-dorm/src/components/pages/PreferencesProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SideBar from '../SideBar'
+import MatchForm from '../MatchForm'
+export default function preferenceProfile() {
+    return (
+        <div>
+            <SideBar/>
+            <MatchForm/>
+        </div>
+    )
+}
---
+++ su-dorm/src/components/pages/SettingsProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from '../Setting'
+import Setting from '../SideBar'
+
+export default function settingsProfile() {
+    return (
+        <div>
+            <SideBar/>
+            <Setting/>
+        </div>
+    )
+}
