5cf30dfa4a798f220f11886d3a5988c4ccc87e60
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-12-12 21:53:20 +0300

    A route for conversations has been added to the api.

---
+++ routes/api/conversations.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const Conversation = require("../models/Conversation");
+
+// @route    POST api/conversations
+// @desc     Create a conversation
+// @access   Private
+
+router.post("/", async (req, res) => {
+  const newConversation = new Conversation({
+    members: [req.body.senderId, req.body.receiverId],
+  });
+
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(200).json(savedConversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// @route    GET api/conversations/:userId
+// @desc     Get conversations of the user with userId
+// @access   Private
+
+router.get("/:userId", async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// @route    GET api/conversations/find/:firstUserId/:secondUserId
+// @desc     Get conversations of 2 users
+// @access   Private
+
+router.get("/find/:firstUserId/:secondUserId", async (req, res) => {
+  try {
+    const conversation = await Conversation.findOne({
+      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+    });
+    res.status(200).json(conversation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
