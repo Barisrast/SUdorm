85d46283b469462c43c9515412cc5b243ef6cd2b
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-12-12 21:56:07 +0300

    routes of messages has been added to the api.

---
+++ routes/api/messages.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const Message = require("../models/Message");
+
+// @route    POST api/messages
+// @desc     add a message
+// @access   Private
+
+router.post("/", async (req, res) => {
+  const newMessage = new Message(req.body);
+
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// @route    get api/messages/:conversationId
+// @desc     get conversation of message
+// @access   Private
+
+router.get("/:conversationId", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
