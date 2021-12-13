908442497ee87e6506635d51b90e35770310ac6a
barisrast <barisrastgeldi@sabanciuniv.edu>
2021-12-13 13:20:09 +0300

    changed text of all profiles page

--- node_modules/client/src/components/profiles/Profiles.js
+++ node_modules/client/src/components/profiles/Profiles.js
@@ -19,10 +19,10 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
           <Spinner />
         ) : (
           <Fragment>
-            <h1 className="large text-primary">Developers</h1>
+            <h1 className="large text-primary">Roommates</h1>
             <p className="lead">
               <i className="fab fa-connectdevelop" /> Browse and connect with
-              developers
+              roommates
             </p>
             <div className="profiles">
               {profiles.length > 0 ? (
